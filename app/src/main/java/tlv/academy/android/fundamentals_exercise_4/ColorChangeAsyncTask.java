3ec842ddd66c7e0831ed846acadfc8fcc12b1a60
yishai <yishai@glide.me>
2017-11-20 10:23:35 +0200

    use hooks to notify activity and change color

--- app/src/main/java/tlv/academy/android/fundamentals_exercise_4/ColorChangeAsyncTask.java
+++ app/src/main/java/tlv/academy/android/fundamentals_exercise_4/ColorChangeAsyncTask.java
@@ -1,6 +1,7 @@
 package tlv.academy.android.fundamentals_exercise_4;
 
 import android.os.AsyncTask;
+import android.os.SystemClock;
 
 /**
  * Created by danielszasz on 19/11/2017.
@@ -16,22 +17,32 @@ public class ColorChangeAsyncTask extends AsyncTask<Integer, Integer, Integer>{
 
     @Override
     protected Integer doInBackground(Integer... aIntegers) {
+        int startColor = aIntegers[0];
+        int endColor = aIntegers[1];
+        while (startColor < endColor) {
+            SystemClock.sleep(200);
+            int nextColor = aIntegers[0] += 10;
+            publishProgress(nextColor);
+        }
         return null;
     }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
+        mIAsyncTaskEvents.onPreExecute();
     }
 
     @Override
     protected void onPostExecute(Integer aInteger) {
         super.onPostExecute(aInteger);
+        mIAsyncTaskEvents.onProgressUpdate(aInteger);
     }
 
     @Override
     protected void onProgressUpdate(Integer... values) {
         super.onProgressUpdate(values);
+        mIAsyncTaskEvents.onProgressUpdate(values[0]);
     }
 
     @Override
